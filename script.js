4737a3ec166dffe4b88c767a4ca9296e8d6592cb
vd6810so <45154600+vd6810so@users.noreply.github.com>
2020-01-16 11:35:37 -0600

    Finished ajax stuff

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-var request = new XMLHttpRequest();
-request.open("GET", "data.txt", false);
+var request;
+
+if (window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+} else {
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+request.open("GET", "data.txt");
+
+request.onreadystatechange = function () {
+    //document.getElementById('demo').innerHTML = request.responseText;
+    if (request.status === 200 && request.readyState === 4) {
+        console.log(request);
+        document.writeln(request.responseText);
+    }
+}
 request.send();
-console.log(request);
-document.getElementById('demo').innerHTML = request.responseText;
